6a1cc675f057104b276cba102a9bfdf5f2b05e46
Pratik Kataria <frek1546@gmail.com>
2018-08-02 09:04:22 +0530

    Created

---
+++ Inventory.java
@@ -0,0 +1,12 @@
+package com.pratik.training.inheritance;
+
+import java.util.ArrayList;
+
+public class Inventory {
+	
+	protected static ArrayList<Products> product_list = new ArrayList<>();
+	
+	public Inventory() {
+		// TODO Auto-generated constructor stub
+	}
+}
